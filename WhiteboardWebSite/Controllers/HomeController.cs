268fc5ae0eae90c3fcfde95314efb7bb124928ac
Shukhrat Mukimov <mukimov@gmail.com>
2011-05-08 16:19:32 +0800

    Initial Project.

---
+++ WhiteboardWebSite/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace WhiteboardWebSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index() {
+            return View();
+        }
+
+        public ViewResult AboutUs() {
+            return View();
+        }
+    }
+}
---
+++ WhiteboardWebSite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WhiteboardWebSite.MvcApplication" Language="C#" %>
