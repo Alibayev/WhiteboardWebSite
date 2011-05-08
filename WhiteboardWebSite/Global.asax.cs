6383e3c8a1765f57dcc20af757d5442a84ec9536
Shukhrat Mukimov <mukimov@gmail.com>
2011-05-09 03:07:11 +0800

    Entity framework was implemented, student, courses, enrollment models was added Database WhiteboardDb succesfully created!

--- WhiteboardWebSite/Global.asax.cs
+++ WhiteboardWebSite/Global.asax.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using WhiteboardWebSite.Models;
 
 namespace WhiteboardWebSite
 {
@@ -65,10 +67,11 @@ namespace WhiteboardWebSite
         }
 
         protected void Application_Start() {
+            Database.SetInitializer(new WhiteboardInitializer());
             AreaRegistration.RegisterAllAreas();
-
             RegisterGlobalFilters(GlobalFilters.Filters);
             RegisterRoutes(RouteTable.Routes);
+            
         }
     }
 }
